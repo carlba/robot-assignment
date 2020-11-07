83804bd9009159e01b8308f4cffa7c30e6d0f608
Carl Bäckström <genzorg@gmail.com>
2020-11-07 10:13:51 +0100

    Add parsing of JSON in request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import * as http from 'http';
 
+interface JSONEnrichedRequest extends http.IncomingMessage {
+  json?: string;
+}
+
 class Server {
   server: http.Server;
   handlers: any[] = [];
@@ -7,9 +11,8 @@ class Server {
     this.server = http.createServer(this.onRequest.bind(this)).listen(port);
   }
 
-  onRequest(req: http.IncomingMessage, res: http.ServerResponse) {
+  async onRequest(req: JSONEnrichedRequest, res: http.ServerResponse) {
     const { url, method } = req;
-    console.log(this.trimLocation(url!));
 
     const handler =
       typeof url === 'string'
@@ -17,6 +20,16 @@ class Server {
             handler => this.trimLocation(url) === handler.path && method === handler.method
           )
         : null;
+
+    if (['POST', 'PUT', 'PATCH'].includes(handler.method)) {
+      try {
+        req.json = await this.getJsonFromRequest(req);
+      } catch (e) {
+        res.statusCode = 422;
+        res.write(e.message);
+        res.end();
+      }
+    }
 
     if (handler) {
       return handler.handler(req, res);
@@ -37,6 +50,25 @@ class Server {
   trimLocation(location: string) {
     return location.replace(/^\/(.*)/g, '$1');
   }
+
+  getJsonFromRequest(req: http.IncomingMessage): Promise<string> {
+    return new Promise((resolve, reject) => {
+      let data = '';
+      req.on('data', chunk => (data += chunk));
+      req.on('end', () => {
+        if (data.length === 0) {
+          reject(new Error('Missing data in request'));
+        }
+        let result: string;
+        try {
+          result = JSON.parse(data);
+          resolve(result);
+        } catch (err) {
+          reject(new Error('Unable to parse JSON data'));
+        }
+      });
+    });
+  }
 }
 
 const server = new Server(3030, 'application/json');
@@ -44,5 +76,9 @@ const server = new Server(3030, 'application/json');
 server.addRoute('robot', 'GET', (req, res) => {
   res.write(JSON.stringify({ message: 'test' }));
   res.statusCode = 200;
+  res.end();
+});
+
+server.addRoute('room', 'POST', (req, res) => {
   res.end();
 });
